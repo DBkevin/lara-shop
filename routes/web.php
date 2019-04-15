41d7cb373f2e02547860b61054bf519faae9e0cf
Leedocker <416606903@outlook.com>
2019-04-15 08:22:11 +0000

    用户界面-商品列表

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-route::get('/','PagesController@root')->name('root')->middleware('verified');
+//route::get('/','PagesController@root')->name('root')->middleware('verified');
+Route::redirect('/', '/products')->name('root');
 Auth::routes(['verify'=>true]);
 //auth 中间件代表需要登陆,verified中间件代表需要验证邮箱
 Route::group(['middleware'=>['auth','verified']],function(){
     Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
     Route::get('user_addresses/create','UserAddressesController@create')->name('user_addresses.create');
     Route::post('user_addresses','UserAddressesController@store')->name('user_addresses.store');
+    Route::get('products', 'ProductsController@index')->name('products.index');
 });
