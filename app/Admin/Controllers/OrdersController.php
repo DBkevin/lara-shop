d2a997494010e369ae1d3cdd8a8c7e3f5103c267
Leedocker <416606903@outlook.com>
2019-04-19 07:26:56 +0000

    管理后台--订单发货

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -8,6 +8,8 @@ use Encore\Admin\Controllers\HasResourceActions;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
+use Illuminate\Http\Request;
+use App\Exceptions\InvalidRequestException;
 
 class OrdersController extends Controller
 {
@@ -39,8 +41,9 @@ class OrdersController extends Controller
         return $content
             ->header('查看订单')
             ->description('查看订单详情')
-            //这样的效果就是页面顶部和左侧都还是 Laravel-Admin 原本的菜单，而页面主要内容就变成了我们这个模板视图渲染的内容了。
-            ->body(view('admin.orders.show',['order'=>$order]));
+            //这样的效果就是页面顶部和左侧都还是 Laravel-Admin 原本的菜单，
+            //而页面主要内容就变成了我们这个模板视图渲染的内容了。
+            ->body(view('admin.orders.show', ['order' => $order]));
     }
 
     /**
@@ -82,31 +85,31 @@ class OrdersController extends Controller
         $grid = new Grid(new Order);
         //只展示已经支付的订单,并且默认按支付时间倒叙排序
 
-        $grid->model()->whereNotNull('paid_at')->orderBy('paid_at','desc');
-        
+        $grid->model()->whereNotNull('paid_at')->orderBy('paid_at', 'desc');
+
         $grid->no('订单流水号');
         //展示关联关系的字段时,使用column方法
-        $grid->column('user.name','买家');
+        $grid->column('user.name', '买家');
         $grid->total_amount('总金额')->sortable();
         $grid->paid_at('支付时间')->sortable();
-        $grid->ship_status('物流')->display(function($value){
+        $grid->ship_status('物流')->display(function ($value) {
             return Order::$shipStatusMap[$value];
         });
-        $grid->refund_status('退款状态')->display(function($value){
+        $grid->refund_status('退款状态')->display(function ($value) {
             return Order::$refundStatusMap[$value];
         });
 
         //禁用创建按钮, 后台不需要创建订单
         $grid->disableCreateButton();
 
-        $grid->actions(function ($actions){
+        $grid->actions(function ($actions) {
             //禁用删除和编辑按钮
             $actions->disableDelete();
             $actions->disableEdit();
         });
-        $grid->tools(function ($tools){
+        $grid->tools(function ($tools) {
             //禁用批量删除按钮
-            $tools->batch(function ($batch){
+            $tools->batch(function ($batch) {
                 $batch->disableDelete();
             });
         });
@@ -172,5 +175,36 @@ class OrdersController extends Controller
         $form->textarea('extra', 'Extra');
 
         return $form;
+    }
+    public function ship(Order $order,Request $request){
+        //判断订单是否已支付
+        if(!$order->paid_at){
+            throw new InvalidRequestException('该订单未付款');
+        }
+
+        //判断当前订单状态是否为未发货
+        if($order->ship_status !== Order::SHIP_STATUS_PENDING){
+            throw new InvalidRequestException('该订单已经发货');
+        }
+        //laravel5.5之后validate方法可以返回校验过的值
+        $data=$this->validate($request,[
+            'express_company'=>['required'],
+            'express_no'=>['required'],
+        ],[],[
+            'express_company'=>'快递名称',
+            'express_no'=>'物流单号',
+        ]);
+
+        //将订单发货状态修改为已发货,并存入物流信息
+        $order->update([
+            'ship_status'=>Order::SHIP_STATUS_DELIVERED,
+            // 我们在 Order 模型的 $casts 属性里指明了 ship_data 是一个数组
+            // 因此这里可以直接把数组传过去
+            'ship_data'=>$data,
+        ]);
+
+
+        //返回上一页
+        return redirect()->back();
     }
 }
