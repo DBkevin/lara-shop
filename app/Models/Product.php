fe0f0a12674f588ac1f600374eb69ddb6b349dbd
Leedocker <416606903@outlook.com>
2019-04-24 07:57:42 +0000

    初始化众筹 模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,10 +7,20 @@ use Illuminate\Support\Str;
 
 class Product extends Model
 {
+
+    const TYPE_NORMAL='normal';
+    const TYPE_CROWDFUNDING='crowdfunding';
+    public static $typeMap=[
+        self::TYPE_NORMAL=>'普通商品',
+        self::TYPE_CROWDFUNDING=>'众筹商品',
+    ];
+
+
     //允许写入的字段
     protected $fillable = [
         'title', 'description', 'image', 'on_sale',
-        'rating', 'sold_count', 'review_count', 'price'
+        'rating', 'sold_count', 'review_count', 'price',
+        'type'
     ];
     //需要转换的类型
     protected $casts = [
@@ -29,7 +39,9 @@ class Product extends Model
         }
         return \Storage::disk('public')->url($this->attributes['image']);
     }
-
+    public function crowdfunding(){
+        return $this->hasOne(CrowdfundingProduct::class);
+    }
     public function category()
     {
         return $this->belongsTo(Category::class);
---
+++ database/migrations/2019_04_24_152303_create_crowdfunding_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCrowdfundingProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('crowdfunding_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('product_id')->comment('关联商品表外键,级联删除');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->decimal('target_amount',10,2)->comment('目标金额');
+            $table->decimal("total_amount",10,2)->default(0)->comment('当前金额');
+            $table->unsignedInteger('user_count')->default(0)->comment('参数人数');
+            $table->dateTime('end_at')->comment('结束时间');
+            $table->string('status')->defaut(\App\Models\CrowdfundingProduct::STATUS_FUNDING)->comment("当前状态,默认值为筹款中");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('crowdfunding_products');
+    }
+}
