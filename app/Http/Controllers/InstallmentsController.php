54dddb41257f70f0c5647ab184a8cef236f647f9
Leedocker <416606903@outlook.com>
2019-05-03 08:26:51 +0000

    前台分期付款详情页

--- app/Http/Controllers/InstallmentsController.php
+++ app/Http/Controllers/InstallmentsController.php
@@ -13,7 +13,17 @@ class InstallmentsController extends Controller
         $installments=Installment::query()
             ->where('user_id',$request->user()->id)
             ->paginate(10);
-
         return  view('installments.index',compact('installments'));
+    }
+
+    public function show(Installment $installment){
+        $this->authorize('own',$installment);
+        //取出当前的还款计划,并按还款排序
+        $items=$installment->items()->orderBy('sequence')->get();
+        return view('installments.show',[
+            'installment'=>$installment,
+            'items'=>$items,
+            'nextItem'=>$items->where('paid_at',null)->first(),
+        ]);
     }
 }
